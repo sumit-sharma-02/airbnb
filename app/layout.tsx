602c2d9a670faaf09d59edbfae444c4e9fbcbc7e
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-04-07 00:37:07 +0530

    Fixed hydration bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import { Nunito } from "next/font/google"
-import './globals.css'
-import Navbar from "./components/navbar/Navbar"
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Holiday Homes & Apartment Rentals - Airbnb',
-}
+  title: "Airbnb",
+  description: "Holiday Homes & Apartment Rentals - Airbnb",
+};
 
 const font = Nunito({
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
