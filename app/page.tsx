537681b25632048c38eb678d8b05e535767eb0c7
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-04-10 00:20:13 +0530

    Added ListingCard component to display all the lisitngs.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-export default function Home() {
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div
-      className="
-        text-rose-500 
-        text-2xl
-      "
-    >
-      Hello Airbnb
-    </div>
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                currentUser={currentUser}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
