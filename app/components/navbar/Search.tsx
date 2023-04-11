65dacd2b6bf555840ff059972242bb426fbd29d7
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-04-11 16:42:46 +0530

    Added filter properties

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,9 +1,13 @@
 "use client";
+
 import { BiSearch } from "react-icons/bi";
+import useSearchModal from "@/app/hooks/useSearchModal";
 
 const Search = () => {
+  const searchModal = useSearchModal();
   return (
     <div
+      onClick={searchModal.onOpen}
       className="
         border-[1px] 
         w-full 
